ea3fe13855ba23a76298c2c3df08707685d73feb
neal-fox <nealfox1@gmail.com>
2018-12-22 14:20:15 -0800

    refactor cheeses ArrayList as a HashMap to add cheeseDescription

--- src/main/java/com/example/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/example/cheesemvc/controllers/CheeseController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping(value="cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+    static HashMap<String, String> cheeses = new HashMap<>();
 
     @RequestMapping(value="")
     public String index(Model model) {
@@ -29,8 +30,8 @@ public class CheeseController {
     }
 
     @RequestMapping(value="add", method=RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName) {
-        cheeses.add(cheeseName);
+    public String processAddCheeseForm(@RequestParam String cheeseName, String cheeseDescription) {
+        cheeses.put(cheeseName, cheeseDescription);
         return "redirect:";
     }
 }
